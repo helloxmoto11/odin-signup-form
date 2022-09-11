ac96d64b4af2922394d18d774b0a8948b0b78951
Manny Caravantes <rhcplive11@gmail.com>
2022-09-11 13:17:53 -0500

    just making another commit.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const mainContainer = document.createElement('div');
 mainContainer.className = 'main-container';
 
 const modal = document.createElement('div');
+
+const img = document.createElement('img');
+img.setAttribute("src", "img/unsplash-photo.jpg");
+
+modal.appendChild(img);
+mainContainer.appendChild(modal);
 const formContainer = document.createElement('div');
 
 root.appendChild(mainContainer);

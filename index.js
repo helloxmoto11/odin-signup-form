28955dcc0bcd28058545aabb6ff873d2e584dc4c
Manny Caravantes <rhcplive11@gmail.com>
2022-09-10 21:08:47 -0500

    refactor

---
+++ index.js
@@ -0,0 +1,11 @@
+const root = document.querySelector('#root');
+
+const mainContainer = document.createElement('div');
+mainContainer.className = 'main-container';
+
+const modal = document.createElement('div');
+const formContainer = document.createElement('div');
+
+root.appendChild(mainContainer);
+
+
